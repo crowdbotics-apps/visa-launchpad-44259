a99edacb7756d42fc03b29db14137d95ce206f8d
Crowdbotics <team@crowdbotics.com>
2023-11-01 09:28:04 +0000

    Initial commit

---
+++ backend/visa_launchpad_44259/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for visa_launchpad_44259 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "visa_launchpad_44259.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "visa-launchpad-44259",
+  url: "https://visa-launchpad-44259.botics.co",
+  api: "https://visa-launchpad-44259.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
